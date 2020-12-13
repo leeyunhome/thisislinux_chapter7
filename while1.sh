a8c87823f015dbbb48441c8c3d4d5a974e65924f
leeyunhome <leeyunhome@daum.net>
2020-12-14 05:44:32 +0900

    upto function

---
+++ while1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+while [ : ]
+do
+	echo "우분투 20.04 LTS"
+done
+exit 0
---
+++ while_sum.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+hap=0
+i=1
+#while [ $i -le 10 ]
+until [ $i -gt 10 ]
+do
+	hap=`expr $hap + $i`
+	i=`expr $i + 1`
+done
+echo "1부터 10까지의 합:"$hap
+exit 0
